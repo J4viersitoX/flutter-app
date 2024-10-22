d25069e8753ef99cb540496ba004a416a46bef9b
Javier <j4viersitox7@gmail.com>
2024-10-22 13:13:08 -0300

    banner principal de estado de consumo y 2 botones sin funcionalidad

--- lib/menu_resumen.dart
+++ lib/menu_resumen.dart
@@ -8,26 +8,107 @@ class MenuResumen extends StatefulWidget {
 }
 
 class _MenuResumenState extends State<MenuResumen> {
+  final _gridBtnStyle = const ButtonStyle(
+                    fixedSize: WidgetStatePropertyAll<Size>(
+                      Size.fromHeight(100.0)
+                    ),
+                    shape: WidgetStatePropertyAll(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20.0))
+                      )
+                    ),
+                    textStyle: WidgetStatePropertyAll(
+                      TextStyle(fontSize: 16.0, ),
+                    ),
+                  );
+
   @override
   Widget build(BuildContext context) {
-    final _vpSize = MediaQuery.of(context).size;
+    final vpSize = MediaQuery.of(context).size;
     return ListView(
       children: [
         Card(
-          child: ListTile(
-            leading: Icon(
-              Icons.tag_faces,
-              color: Colors.red,
-              size: _vpSize.width * 0.6,
-            ),
-            title: TextButton(
-                onPressed: () {},
-                child: Text(
-                  "Ver acciones pendientes",
-                  style: TextStyle(fontSize: 17.0),
-                ))
+          margin: const EdgeInsets.all(12.0),
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(20.0))
+          ),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Icon(
+                  Icons.tag_faces,
+                  color: Colors.red,
+                  size: vpSize.width * 0.3,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Column(
+                  children: [
+                    const Text("Mal consumo eléctrico!!!", style: TextStyle(fontSize: 19.0),),
+                    IconButton(
+                      padding: const EdgeInsets.symmetric(vertical: 32.0, horizontal: 16.0),
+                      onPressed: () {},
+                      icon: const SizedBox(
+                        width: 160.0,
+                        child: Row(
+                          children: [
+                            Expanded(
+                              flex: 3,
+                              child: Text(
+                                "Ver acciones\npendientes",
+                                style: TextStyle(fontSize: 17.0),
+                              ),
+                            ),
+                            Expanded(child: Icon(Icons.arrow_right)),
+                          ],
+                        ),
+                      )
+                    )
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
+        Row(
+          children: [
+            Expanded(
+              child: Container(
+                margin: const EdgeInsets.all(12.0),
+                child: ElevatedButton(
+                  style: _gridBtnStyle,
+                  onPressed: () {},
+                  child: const Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Icon(Icons.devices),
+                      Text("Ver aparatos\nregistrados", textAlign: TextAlign.center),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                margin: const EdgeInsets.all(12.0),
+                child: ElevatedButton(
+                  style: _gridBtnStyle,
+                  onPressed: () {},
+                  child: const Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Icon(Icons.add_box_rounded),
+                      Text("Añadir aparatos", textAlign: TextAlign.center)
+                    ],
+                  )
+                ),
+              ),
+            )
+          ]
+        )
       ],
     );
   }
