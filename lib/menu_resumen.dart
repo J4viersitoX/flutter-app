2db25f90bf04ad7420d119b53e7bdebf5567a807
Javier <j4viersitox7@gmail.com>
2024-11-02 15:20:33 -0300

    estados de uso de energia

--- lib/menu_resumen.dart
+++ lib/menu_resumen.dart
@@ -2,6 +2,74 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app/lista_aparatos.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
+class EstadoEnergia extends StatelessWidget {
+  const EstadoEnergia({
+    super.key,
+    required this.icon,
+    required this.title,
+    required this.body
+  });
+
+  final String icon;
+  final String title;
+  final String body;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 180,
+      child: Card(
+        margin: const EdgeInsets.all(12.0),
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(20.0))
+        ),
+        color: Theme.of(context).colorScheme.primaryContainer,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              flex: 1,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 12.0),
+                child: SvgPicture.asset(icon),
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      title,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 19.0,
+                        color: Theme.of(context).colorScheme.onPrimaryContainer,
+                        fontWeight: FontWeight.bold
+                      )
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      body,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        color: Theme.of(context).colorScheme.onPrimaryContainer
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
 class MenuResumen extends StatefulWidget {
   const MenuResumen({super.key});
 
@@ -10,84 +78,47 @@ class MenuResumen extends StatefulWidget {
 }
 
 class _MenuResumenState extends State<MenuResumen> {
+  final _estados = <EstadoEnergia>[
+    const EstadoEnergia(
+      icon: "assets/images/Carita Feli.svg",
+      title: "¡Excelente Trabajo!",
+      body: "Su consumo de energía es óptimo",
+    ),
+    const EstadoEnergia(
+      icon: "assets/images/Carita Seria.svg",
+      title: "Sugerencia",
+      body: "Su consumo de energía puede mejorar",
+    ),
+    const EstadoEnergia(
+      icon: "assets/images/Carita Muerta.svg",
+      title: "¡Advertencia!",
+      body: "Su consumo de energía tiene mucho margen de mejora",
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     final gridBtnStyle = ButtonStyle(
-                  fixedSize: const WidgetStatePropertyAll<Size>(
-                    Size.fromHeight(100.0)
-                  ),
-                  shape: const WidgetStatePropertyAll(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(20.0))
-                    )
-                  ),
-                  textStyle: const WidgetStatePropertyAll(
-                    TextStyle(fontSize: 16.0, ),
-                  ),
-                  foregroundColor: WidgetStatePropertyAll(Theme.of(context).colorScheme.onPrimaryContainer),
-                  backgroundColor: WidgetStatePropertyAll(Theme.of(context).colorScheme.primaryContainer)
-                );
+      fixedSize: const WidgetStatePropertyAll<Size>(
+        Size.fromHeight(100.0)
+      ),
+      shape: const WidgetStatePropertyAll(
+        RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(20.0))
+        )
+      ),
+      textStyle: const WidgetStatePropertyAll(
+        TextStyle(fontSize: 16.0, ),
+      ),
+      foregroundColor: WidgetStatePropertyAll(Theme.of(context).colorScheme.onPrimaryContainer),
+      backgroundColor: WidgetStatePropertyAll(Theme.of(context).colorScheme.primaryContainer)
+    );
+
     final vpSize = MediaQuery.of(context).size;
+
     return ListView(
       children: [
-        Card(
-          margin: const EdgeInsets.all(12.0),
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(20.0))
-          ),
-          color: Theme.of(context).colorScheme.primaryContainer,
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Expanded(
-                flex: 1,
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 12.0),
-                  child: SvgPicture.asset("assets/images/Carita Muerta.svg"),
-                ),
-              ),
-              Expanded(
-                flex: 2,
-                child: Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: Column(
-                    children: [
-                      Text(
-                        "Mal consumo eléctrico!!!",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(fontSize: 19.0, color: Theme.of(context).colorScheme.onPrimaryContainer)
-                      ),
-                      IconButton(
-                        padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
-                        onPressed: () {},
-                        icon: SizedBox(
-                          width: 160.0,
-                          child: Row(
-                            children: [
-                              Expanded(
-                                flex: 3,
-                                child: Text(
-                                  "Ver acciones\npendientes",
-                                  style: TextStyle(fontSize: 17.0, color: Theme.of(context).colorScheme.onPrimaryContainer),
-                                ),
-                              ),
-                              Expanded(
-                                child: Icon(
-                                  Icons.arrow_right,
-                                  color: Theme.of(context).colorScheme.onPrimaryContainer
-                                )
-                              ),
-                            ],
-                          ),
-                        )
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
+        _estados[0],
         Row(
           children: [
             Expanded(
